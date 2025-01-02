a3c5375c1c3bdfc6b680583f580b96205d2baff4
keerthanamoorthy9687 <102482438+keerthanamoorthy9687@users.noreply.github.com>
2025-01-02 13:58:42 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Content from './Component/Content';
+
+const App  = () => {
+  return (
+    <>    
+     <div className="contents"><Content/></div>
+      
+    </>
+
+   
+  )
+}
+export default App;
+
